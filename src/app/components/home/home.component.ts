b38e60e24ab6c3c6cee12c9b5116e20b922f6a7a
JavierCoca95 <javiercocamembribes95@gmail.com>
2023-07-28 11:33:41 +0200

    ahora el menu viene de un mock, tambien sigo con correciones de whatsapp y github

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { AppService } from 'src/app/services/appservice.service';
+import { Link } from 'src/app/models/link.model';
+import { Subscription} from 'rxjs';
 
 
 @Component({
@@ -8,5 +11,34 @@ import { Component, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None,
 })
 export class HomeComponent {
+  links: Link[] = [];
+  linkSub: Subscription = new Subscription();
+
+  constructor(private appservice: AppService) {}
+
+  ngOnDestroy() {
+    this.linkSub?.unsubscribe();
+    console.log('Se ha desuscrito');
+
+  }
+
+  ngOnInit() : void {
+    this.linkSub = this.appservice.getLinks().subscribe({
+      next: (res: Link[]) => {
+        this.links = res;
+      },
+      error: (error: any) => {
+        console.error('Error occurred:', error);
+      },
+      complete: () => {
+        console.log('Observable completed');
+
+      }
+  })
+  }
+
+
 
 }
+
+
--- src/app/services/appservice.service.ts
+++ src/app/services/appservice.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable} from 'rxjs';
 import { Project } from '../models/project.model';
+import { Link } from '../models/link.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,9 @@ export class AppService {
 
   constructor(private http: HttpClient) { }
 
+  getLinks() : Observable<Link[]> {
+    return this.http.get<Link[]>(`assets/mocks/links.mock.json?v=` + Date.now())
+  }
 
   getProjects() : Observable<Project[]>  {
     return this.http.get<Project[]>(`assets/mocks/projects.json?v=` + Date.now());
